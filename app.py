136c174b00e806bf4d37d88788898890ce926ec6
Meghana Doradla <meghanadoradla@gmail.com>
2025-02-05 13:02:04 +0530

    first commit

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ working_dir=os.path.dirname(os.path.abspath(__file__))
 
 diabetes_model=pickle.load(open(f'{working_dir}/saved_models/diabetes_model.sav','rb'))
 
-heart_disease_model=pickle.load(open(f'{working_dir}(/saved_models/heart_disease_model.sav','rb'))
+heart_disease_model=pickle.load(open(f'{working_dir}/saved_models/heart_disease_model.sav','rb'))
 
 parkinsons_disease_model=pickle.load(open(f'{working_dir}/saved_models/parkinsons_disease_model.sav','rb'))
 
